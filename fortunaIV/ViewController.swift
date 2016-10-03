314495e1721cc0c8b36cc6bb3d8393ef4ad97d8c
Frank Robben <frankrobben@ptr-2hj4tbhtrle5uyen4bzbvcsp8.ip6.access.telenet.be>
2016-10-03 10:32:19 +0200

    Initial Commit

---
+++ fortunaIV/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  fortunaIV
+//
+//  Created by Frank Robben on 3/10/16.
+//  Copyright © 2016 Freebird Fasthacks. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
